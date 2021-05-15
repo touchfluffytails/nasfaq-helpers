2307c978cf867cf8b1ccfd59b976bcb717990585
touchfluffytails <73208262+touchfluffytails@users.noreply.github.com>
2021-05-15 19:47:12 +1000

    Add files via upload

---
+++ holodivs.py
@@ -0,0 +1,224 @@
+import sys
+import json
+import requests
+import time
+import datetime
+import csv
+
+
+# from colorama import init
+# from colorama import Fore, Style
+# init()
+
+
+HOLO_DIVS_URL = "https://nasfaq.biz/api/getDividends"
+HOLO_STATS_URL = "https://nasfaq.biz/api/getStats"
+
+SEVENTEEN_HOURS = 61200
+
+HOLOS = ["hololive", "sora", "roboco", "miko", "suisei", "azki", "mel", "fubuki", "matsuri", "aki", "haato", "aqua", "shion", "ayame", "choco", "choco_alt", "subaru", "mio", "okayu", "korone", "pekora", "rushia", "flare", "noel", "marine", "kanata", "coco", "watame", "towa", "himemoriluna", "lamy", "nene", "botan", "polka", "risu", "moona", "iofi", "calliope", "kiara", "inanis", "gura", "amelia", "ollie", "melfissa", "reine", "ui", "nana", "pochimaru", "ayamy", "civia"]
+HOLOS = ["hololive", "sora", "roboco", "miko", "suisei", "azki", "mel", "fubuki", "matsuri", "aki", "haato", "aqua", "shion", "ayame", "choco", "choco_alt", "subaru", "mio", "okayu", "korone", "pekora", "rushia", "flare", "noel", "marine", "kanata", "coco", "watame", "towa", "himemoriluna", "lamy", "nene", "botan", "polka", "risu", "moona", "iofi", "calliope", "kiara", "inanis", "gura", "amelia", "ollie", "melfissa", "reine", "ui", "nana", "civia"]
+
+
+
+
+divResponse = requests.get(HOLO_DIVS_URL)
+statResponse =requests.get(HOLO_STATS_URL)
+
+# with open("data_file.json", "w") as write_file:
+#     json.dump(data, write_file)
+
+# currentTime = int(time.time())
+
+currentDate = datetime.datetime.now()
+
+lastFriday = (currentDate.date() - datetime.timedelta(days=currentDate.weekday()) + datetime.timedelta(days=4, weeks=-1))
+
+
+divsList = divResponse.json()['dividends']['payouts']
+holoStats = statResponse.json()['stats']
+
+dateFormat = "{month}/{day}/{year}"
+lastFridayDateStamp = dateFormat.format(month = lastFriday.strftime("%m"), day = lastFriday.strftime("%d"), year = lastFriday.strftime("%Y"))
+currentDateStamp = dateFormat.format(month = currentDate.strftime("%m"), day = currentDate.strftime("%d"), year = currentDate.strftime("%Y"))
+
+# divs
+divs = {}
+with open("holodivs.json", "r+") as jsonFile:
+    divs = json.load(jsonFile)
+    curDivTime = int(divResponse.json()['dividends']['timestamp'])
+
+    if str(curDivTime) in divs:
+        pass
+    else:
+        divs[curDivTime] = divsList
+        jsonFile.seek(0)
+        json.dump(divs, jsonFile)
+lastWeekDivTime = 0
+for divTime in divs:
+    if(int(divTime) > time.mktime(lastFriday.timetuple()) + SEVENTEEN_HOURS):
+        lastWeekDivTime = divTime
+lastWeekDivs = divs[lastWeekDivTime]
+
+
+# current prices
+todayPrices = statResponse.json()['todayPrices'][-1]['coinInfo']['data']
+
+goodHolos = {}
+acceptableHolos= {}
+with open("holodivs.txt", "w") as holoDivFile:
+
+    for holo in HOLOS:
+        if (holo == 'civia' or holo == "choco_alt"):
+            continue
+        divPayout = 0
+        if (holo in divsList.keys()):
+            divPayout = divsList[holo]
+
+        prevFridayIndex = 0
+        curDateIndex = 0
+
+        weeklyViewStats = holoStats[holo]['weeklyViewCount']
+        weeklySubsStats = holoStats[holo]['weeklySubscriberCount']
+        dateLabels = weeklyViewStats['labels']
+
+        for date in range(len(dateLabels)):
+            if (dateLabels[date] == lastFridayDateStamp):
+                prevFridayIndex = date
+            elif (dateLabels[date] == currentDateStamp):
+                curDateIndex = int(date)
+
+        prevFridayWeeklyViews = int(weeklyViewStats['data'][prevFridayIndex])
+        curWeeklyViews = int(weeklyViewStats['data'][curDateIndex])
+
+        prevFridayWeeklySubs = int(weeklySubsStats['data'][prevFridayIndex])
+        curWeeklySubs = int(weeklySubsStats['data'][curDateIndex])
+        if (holo == "choco"):
+            pass
+            chocoAlt = holoStats['choco_alt']
+            prevFridayWeeklyViews += int(chocoAlt['weeklyViewCount']['data'][prevFridayIndex])
+            curWeeklyViews += int(chocoAlt['weeklyViewCount']['data'][curDateIndex])
+
+            prevFridayWeeklySubs += int(chocoAlt['weeklySubscriberCount']['data'][prevFridayIndex])
+            curWeeklySubs += int(chocoAlt['weeklySubscriberCount']['data'][curDateIndex])
+
+        if (lastWeekDivs[holo] / todayPrices[holo]['price'] >= 0.30):
+            pass
+            normDiv = (lastWeekDivs[holo] / todayPrices[holo]['price'])
+            div10 = (lastWeekDivs[holo] * 1.1) / todayPrices[holo]['price']
+            goodHolos[holo] = {"div": normDiv, "div10": div10,"price":lastWeekDivs[holo]}
+        elif ((lastWeekDivs[holo] * 1.1) / todayPrices[holo]['price'] >= 0.30):
+            pass
+            normDiv = (lastWeekDivs[holo] / todayPrices[holo]['price'])
+            div10 = (lastWeekDivs[holo] * 1.1) / todayPrices[holo]['price']
+            acceptableHolos[holo] = {"div": normDiv, "div10": div10,"price":lastWeekDivs[holo]}
+
+        def PrintStats():
+            # print (Fore.BLUE)
+
+            print(holo)
+
+            # print(Style.RESET_ALL)
+
+            print ( "\nPF views - " + str(prevFridayWeeklyViews))
+            print ( "CW views - " + str(curWeeklyViews))
+            print ("View count difference - " + str(curWeeklyViews - prevFridayWeeklyViews))
+            print ("Views % difference - " + str(((curWeeklyViews / prevFridayWeeklyViews) - 1) * 100) + "%\n")
+
+            print ( "PF subs - " + str(prevFridayWeeklySubs))
+            print ( "CW subs - " + str(curWeeklySubs))
+            print ("Subs count difference - " + str(curWeeklyViews - prevFridayWeeklySubs))
+            if (curWeeklySubs != 0):
+                print ("Subs % difference - " + str(((curWeeklySubs / prevFridayWeeklySubs) - 1) * 100)+ "%\n")
+
+            print ("Last week divs - " + str(lastWeekDivs[holo]))
+            # print(Fore.GREEN)
+            print ( "Div/coin price - " + str(lastWeekDivs[holo] / todayPrices[holo]['price'] ))
+
+            # print(Style.RESET_ALL)
+
+            print ("+10% div/last week - " + str(lastWeekDivs[holo] * 1.1))
+            # print(Fore.RED)
+            print (("+10% div/coin price - " + str((lastWeekDivs[holo] * 1.1) / todayPrices[holo]['price'])))
+            
+            # print(Style.RESET_ALL)
+
+            print("\n--------------------------------------\n")
+
+        PrintStats()
+        orgStdout = sys.stdout
+        sys.stdout = holoDivFile
+        PrintStats()
+        sys.stdout = orgStdout
+
+    def PrintGoodHolos(holo):
+        div = goodHolos[holo]['div']
+        div10 = goodHolos[holo]['div10']
+        printStatement = "{holo} - div {div} - 10% {div10}".format(holo=holo, div=div, div10=div10)
+        print(printStatement)
+        print("Last week div price - {price}".format(price=goodHolos[holo]['price']))
+        printStatement = "300k/current coin price/div - {totalDivCost}"
+        totalDivCost = (300000/todayPrices[holo]['price']) * lastWeekDivs[holo]
+        print(printStatement.format(totalDivCost=totalDivCost))
+
+        printStatement = "300k/current coin price/10% div - {totalDivCost}\n"
+        totalDivCost = (300000/todayPrices[holo]['price']) * (lastWeekDivs[holo] *1.1)
+        print(printStatement.format(totalDivCost=totalDivCost))
+
+
+    print("Good Holos\n")
+    
+    sys.stdout = holoDivFile
+    print("Good Holos\n")
+    sys.stdout = orgStdout
+
+    # TODO Clean up the vars so its easier to read
+    for holo in goodHolos:
+        pass
+        PrintGoodHolos(holo)
+        orgStdout = sys.stdout
+        sys.stdout = holoDivFile
+        PrintGoodHolos(holo)
+        sys.stdout = orgStdout
+
+
+    print("\n--------------------------------------\n")
+
+
+    print ("Acceptable Holos\n")
+    sys.stdout = holoDivFile
+    print ("Acceptable Holos\n")
+    sys.stdout = orgStdout
+
+    def PrintAcceptableHolos(holo):
+        div = acceptableHolos[holo]['div']
+        div10 = acceptableHolos[holo]['div10']
+        printStatement = "{holo} - div {div} - 10% {div10}".format(holo=holo, div=div, div10=div10)
+        print(printStatement)
+        print("Last week div price - {price}".format(price=acceptableHolos[holo]['price']))
+        printStatement = "300k/current coin price/div - {totalDivCost}"
+        totalDivCost = (300000/todayPrices[holo]['price']) * lastWeekDivs[holo]
+        print(printStatement.format(totalDivCost=totalDivCost))
+
+        printStatement = "300k/current coin price/10% div - {totalDivCost}\n"
+        totalDivCost = (300000/todayPrices[holo]['price']) * (lastWeekDivs[holo] *1.1)
+        print(printStatement.format(totalDivCost=totalDivCost))
+
+
+    for holo in acceptableHolos:
+        pass
+        PrintAcceptableHolos(holo)
+        orgStdout = sys.stdout
+        sys.stdout = holoDivFile
+        PrintAcceptableHolos(holo)
+        sys.stdout = orgStdout
+
+# with open('output.csv', 'wb') as output:
+#     writer = csv.writer(output)
+#     for key, value in goodHolos.iteritems():
+# #         writer.writerow([key, value])
+
+# import openpyxl
+
+# df= pd.DataFrame.from_dict(goodHolos, orient='index')
+# df.to_excel(r"output.xlsx",sheet_name='Sheet_name')
---
+++ holostats.py
@@ -0,0 +1,200 @@
+import sys
+import json
+import requests
+import time
+from datetime import datetime,timedelta
+
+# from colorama import init
+# from colorama import Fore, Style
+# init()
+
+## TODO Add in share differences
+## TODO Modify % to * 100
+HOLO_POI_URL = "https://holo.poi.cat/api/v4/channels_report?ids={holo}&metrics=youtube_channel_subscriber,youtube_channel_view&startAt={startDate}&endAt={endDate}"
+NASFAQ_URL = "https://nasfaq.biz/api/getStats"
+
+LOCAL_RESET_HOUR = 15
+LOCAL_RESET_MINUTE = 0
+
+# subs are possibly worth about 10k view for 1k subs
+# this doesn't apply anymore but I'm too lazy to scrub it out
+SUB_TO_VIEW_VALUE = 10000
+
+# list the holos by their generation according to nasfaq since
+# the lists returned by neither server does so
+HOLOS = ["hololive", "sora", "roboco", "miko", "suisei", "azki", "mel", "fubuki", "matsuri", "aki", "haato", "aqua", "shion", "ayame", "choco", "choco_alt", "subaru", "mio", "okayu", "korone", "pekora", "rushia", "flare", "noel", "marine", "kanata", "coco", "watame", "towa", "himemoriluna", "lamy", "nene", "botan", "polka", "risu", "moona", "iofi", "calliope", "kiara", "inanis", "gura", "amelia", "ollie", "melfissa", "reine", "ui", "nana", "pochimaru", "ayamy", "civia"]
+
+
+# testHolo = "kanata"
+
+# with open("data_file.json", "w") as write_file:
+#     json.dump(data, write_file)
+currentTime = int(time.time() * 1e3) 
+threeDaysMilliSeconds = 259200000
+
+threeDaysAgoTime = int((currentTime - threeDaysMilliSeconds))
+# testHolo = "kanata"
+# testHoloURL = HOLO_POI_URL.format(holo=testHolo, startDate=threeDaysAgoTime, endDate=currentTime)
+# print(testHoloURL)
+# response = requests.get(testHoloURL)
+
+statResponse = requests.get(NASFAQ_URL)
+
+currentDate = datetime.now()
+dateFormat = "{month}/{day}/{year}"
+
+currentDateStamp = dateFormat.format(month = currentDate.strftime("%m"), day = currentDate.strftime("%d"), year = currentDate.strftime("%Y"))
+
+holoList = statResponse.json()['stats']
+poiStatCount ={}
+
+yesterdayAdjustmentTime = statResponse.json()['todayPrices'][0]['coinInfo']['timestamp']
+yesterdayAdjustmentHistory = {}
+for tick in reversed(range(len(statResponse.json()['coinHistory']))):
+    pass
+    history = statResponse.json()['coinHistory']
+    if(history[tick]['timestamp'] == yesterdayAdjustmentTime):
+        pass
+        yesterdayAdjustmentHistory = history[tick]['data']
+todayCoinHistory = statResponse.json()['todayPrices'][-1]["coinInfo"]['data']
+
+
+
+
+for holo in HOLOS:
+    if (holo == 'civia'):
+        continue
+    holoPoiUrl = HOLO_POI_URL.format(holo=holo, startDate=threeDaysAgoTime, endDate=currentTime)
+    poiStatReponse = requests.get(holoPoiUrl)
+    # print(poiStatReponse.json())
+    nasDailySubCount = holoList[holo]['dailySubscriberCount']['data'][-1]
+    nasDailyViewCount = holoList[holo]['dailyViewCount']['data'][-1]
+
+    holoPoiStatCount = {}
+
+    yesterday = currentDate - timedelta(days=1)
+    twoDaysAgo = currentDate - timedelta(days=2)
+
+    for report in poiStatReponse.json()['reports']:
+        pass
+        # print(report)
+        # print('\n\n\n\n')
+        rows = report['rows']
+        poiTodayCount = {"time":rows[-1][0], "value":rows[-1][1]}
+        poiYesterdayCount = {}
+        poiTwoDaysCount ={}
+        poiDailyCount = 0
+
+        statCounts = {}
+        statCounts['today'] = poiTodayCount
+
+        for row in reversed(range(len(rows)-1)):
+            pass
+            time = rows[row][0]
+            unixTime = time/1e3
+            # print(unixTime)
+            date = datetime.fromtimestamp(unixTime)
+            day = date.day
+            hour = date.hour
+            minutes = date.minute
+            # print(date)
+            # print(day == yesterday.day)
+            # print(hour == LOCAL_RESET_HOUR)
+            # print(minutes == LOCAL_RESET_MINUTE)
+            if (day == yesterday.day and hour == LOCAL_RESET_HOUR and minutes == LOCAL_RESET_MINUTE):
+                poiYesterdayCount = {"time":rows[row][0], "value":rows[row][1]}
+                statCounts['yesterday'] = poiYesterdayCount
+
+            elif(day == twoDaysAgo.day and hour == LOCAL_RESET_HOUR and minutes == LOCAL_RESET_MINUTE):
+                poiTwoDaysCount = {"time":rows[row][0], "value":rows[row][1]}
+                statCounts['twoDays'] = poiTwoDaysCount
+
+        if (report['kind'] == "youtube_channel_subscriber"):
+            pass
+            holoPoiStatCount['subs'] = statCounts
+        elif (report['kind'] == 'youtube_channel_view'):
+            pass
+            holoPoiStatCount['views'] = statCounts
+    poiStatCount[holo] = holoPoiStatCount
+    # break
+
+with open('holostats.txt', 'w') as statFile:
+
+    def PrintStats(holo):
+        holoPoiStatCount = poiStatCount[holo]
+
+        todayViews = holoPoiStatCount['views']['today']['value']
+        yesterdayViews = holoPoiStatCount['views']['yesterday']['value']
+        twoDaysViews = holoPoiStatCount['views']['twoDays']['value']
+
+        todaySubs = holoPoiStatCount['subs']['today']['value']
+        yesterdaySubs = holoPoiStatCount['subs']['yesterday']['value']
+        twoDaysSubs = holoPoiStatCount['subs']['twoDays']['value']
+
+        dailyViewDifference = todayViews - yesterdayViews
+        yesterdayViewDifference = yesterdayViews - twoDaysViews
+        dailySubDifference = todaySubs - yesterdaySubs
+        yesterdaySubDifference = yesterdaySubs - twoDaysSubs
+
+        theoreticalSubViews = (dailySubDifference / 1000) * SUB_TO_VIEW_VALUE
+        theoreticalYesterdaySubViews = (yesterdaySubDifference / 1000) * SUB_TO_VIEW_VALUE
+
+
+        # print(todayViews)
+        # print(yesterdayViews)
+        # print(dailyViewDifference)
+
+        # print (Fore.BLUE)
+
+        print(holo)
+
+        # print(Style.RESET_ALL)
+
+        holoPoiStatCount=poiStatCount[holo]
+
+        if(holo == "choco"):
+            todayViews += poiStatCount['choco_alt']['views']['today']['value']
+            yesterdayViews += poiStatCount['choco_alt']['views']['yesterday']['value']
+            todaySubs += poiStatCount['choco_alt']['views']['today']['value']
+            yesterdaySubs += poiStatCount['choco_alt']['views']['yesterday']['value']
+
+        print("Today views - {views}".format(views=todayViews))
+        print("Yesterday views - {views}".format(views=yesterdayViews))
+        print("Daily difference - {difference}".format(difference=(dailyViewDifference)))
+        print("Yesterday daily difference - {difference}".format(difference=(yesterdayViewDifference)))
+        print("Day difference - {difference}".format(difference=(dailyViewDifference - yesterdayViewDifference)))
+        print("Daily % difference - {difference}%".format(difference=((float(dailyViewDifference / yesterdayViewDifference) -1) * 100)))
+        print("Theoretical daily views with sub views - {views}".format(views=((dailyViewDifference) + theoreticalSubViews)))
+        print("Theoretical yesterday daily views with sub views - {views}".format(views=((yesterdayViewDifference) + theoreticalYesterdaySubViews)))
+        print("Daily difference with theoretical views - {difference}".format(difference=((dailyViewDifference + theoreticalSubViews) - (yesterdayViewDifference + theoreticalYesterdaySubViews))))
+        print("Daily % difference with theoretical views - {difference}".format(difference=(((float(dailyViewDifference + theoreticalSubViews) / (yesterdayViewDifference + theoreticalYesterdaySubViews)) -1) * 100)))
+
+        print("\nToday coin circulation - {circulated}".format(circulated=todayCoinHistory[holo]['inCirculation']))
+        print("Yesterday coin circulation - {circulated}".format(circulated=yesterdayAdjustmentHistory[holo]['inCirculation']))
+        print("Daily coin circulation difference - {circulated}".format(circulated=todayCoinHistory[holo]['inCirculation'] - yesterdayAdjustmentHistory[holo]['inCirculation']))
+        print("Daily coin % circulation difference - {circulated}%".format(circulated=((todayCoinHistory[holo]['inCirculation'] / yesterdayAdjustmentHistory[holo]['inCirculation']) -1) * 100))
+
+
+
+
+
+
+
+        print("\nToday subs - {views}".format(views=todaySubs))
+        print("Yesterday subs - {views}".format(views=yesterdaySubs))
+        print("Daily difference - {difference}".format(difference=(dailySubDifference)))
+        print("Yesterday daily difference - {difference}".format(difference=(yesterdaySubDifference)))
+        if(yesterdaySubDifference != 0):
+            print("Daily % difference - {difference}%".format(difference=((float(dailySubDifference / yesterdaySubDifference) - 1) * 100)))
+
+        print ("\n---------------------------------")
+
+
+    for holo in poiStatCount:
+        if(holo =="choco_alt"):
+            continue
+        PrintStats(holo)
+        orgStdout = sys.stdout
+        sys.stdout = statFile
+        PrintStats(holo)
+        sys.stdout = orgStdout
